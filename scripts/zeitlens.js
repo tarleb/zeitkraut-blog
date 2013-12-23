f8cd6f21521d8b5632744e73dc020740f40cd3c8
Albert Krewinkel <tarleb@moltkeplatz.de>
2013-12-23 17:30:17 +0100

    Remove jQuery dependency from zeitlens.js Fix execution of javascript on page load

--- scripts/zeitlens.js
+++ scripts/zeitlens.js
@@ -1,11 +1,43 @@
-jQuery("document").ready(function($) {
-  // convert obfuscated mail addresses into clickable "mailto:" links
-  var mailAddressClass = "obfuscated-mail-address";
-  $("."+mailAddressClass).each(function() {
-    if (!$(this).hasClass("dont-touch")) {
-      var address = $(this).attr("data-user") + "@" + $(this).attr("data-domain");
-      $(this).html($("<a href=\"mailto:" + address + "\">" + address + "</a>", {}));
-      $(this).removeClass(mailAddressClass);
+var zeitlens = (function() {
+  function removeAllChildNodes(node) {
+    while (node.firstChild) {
+      node.removeChild(node.firstChild);
     }
-  });
+    return node;
+  };
+
+  function hasClass(node, className) {
+    return node.className.match(new RegExp("\\b" + className + "\\b"));
+  }
+
+  function createMailtoLink(address) {
+    var link = document.createElement("a");
+    link.href = "mailto:" + address;
+    link.textContent = address;
+    return link;
+  }
+
+  // convert obfuscated mail addresses into clickable "mailto:" links
+  deobfuscateMailAddresses = function(mailAddressSelector, dontTouchClass) {
+    var obfuscatedAddresses = document.querySelectorAll(mailAddressSelector);
+    for (var i = 0; i < obfuscatedAddresses.length; i++) {
+      var node = obfuscatedAddresses[i];
+      console.log(node);
+      if (!hasClass(node, dontTouchClass)) {
+        removeAllChildNodes(node);
+        node.className = "";
+        var address = node.dataset.user + "@" + node.dataset.domain;
+        node.appendChild(createMailtoLink(address));
+      }
+    }
+  };
+
+  return {
+    deobfuscateMailAddresses: deobfuscateMailAddresses
+  };
+})();
+
+document.addEventListener("readystatechange", function(event) {
+  var mailAddressClass = "obfuscated-mail-address";
+  zeitlens.deobfuscateMailAddresses(".obfuscated-mail-address", "dont-touch");
 });
